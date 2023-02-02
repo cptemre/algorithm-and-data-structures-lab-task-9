efdcfdb2b9d3a7c0bd9a8403cecc2a979f43b720
cptemre <kunduraci2@gmail.com>
2023-02-02 12:39:07 +0100

    Project Upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# ALGORITHMS AND DATA STRUCTURES LABORATORY
+
+## TASK 9
+
+### GENERAL INFORMATION
+
+1. This project has been prepared for a course in informatics in the first semester.
+
+2. Project is prepared in Visual Studio 2022 application in Windows 11 OS. It may not work as intended in other platforms.
+
+3. Project only contains the code in github not the project folder prepared by Visual Studio.
+
+### DESCRIPTION OF THE TASK
+
+Show on the diagram the algorithm for searching a 4x4 element array.
+Enter the contents of the array in the program cone. Use one line of code and a set statement
+[5,7,13,22,
+88,100,3,2,
+33.7.2.3
+7,3,15,2]
+
+Search sequentially on a rectangular area specified by the user
+e.g.
+[0,0,0,0,
+0,*,*,*,
+0,*,*,*,
+0.0.0.0]
+Search for key - the result is all the values ​​in the array corresponding to the key and theirs
+indices.
---
+++ task-9.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int key, rowCounter, rowStart, columnStart, columnEnd = 0;
+    int rowEnd, answerRow = 0;
+    vector<string> answer;
+
+    /*LIST DECLARATION*/
+    vector<vector<int>> myList{
+        {5,7,13,22},
+        {88,100,3,2},
+        {33,7,2,3},
+        {7,3,15,2}
+    };
+
+    /*KEY DECLARATION*/
+    cout << "Please enter the key which you want to search: ";
+    cin >> key;
+    cout << "Please enter in how many rows you will search your key: ";
+    cin >> rowCounter;
+    cout << "\nIndex numbers will be between 0-3...";
+    cout << "\nPlease enter the row which you want to start searching your key: ";
+    cin >> rowStart;
+    cout << "\nPlease enter the row which you want to end searching your key: ";
+    cin >> rowEnd;
+    for (int i = rowStart; i < rowEnd + 1; i++)
+    {
+        cout << "\nPlease enter the starting column for this row which you want to search your key: ";
+        cin >> columnStart;
+        cout << "\nPlease enter the ending column for this row which you want to search your key: ";
+        cin >> columnEnd;
+        for (int j = columnStart; j < columnEnd + 1; j++)
+        {
+            if (myList[i][j] == key)
+            {
+                string text = "Row: " + to_string(i) + " Column: " + to_string(j) + " Key: " + to_string(key);
+                answer.push_back(text);
+            };
+        }
+    }
+
+    for (int i = 0; i < answer.size(); i++)
+    {
+        cout << endl << answer.at(i);
+    }
+
+    return 0;
+}
